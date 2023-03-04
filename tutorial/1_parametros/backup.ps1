5c3144a6fe18c4f9ec4190f84c907c61b7595642
eugenio <egsmartin@gmail.com>
2023-03-04 16:05:39 +0100

    condiciones

--- tutorial/1_parametros/backup.ps1
+++ tutorial/1_parametros/backup.ps1
@@ -3,6 +3,24 @@ Param(
   [string]$DestinationPath = './'
 )
 
+#Comprueba si existe la ruta
+If (-Not (Test-Path $Path)) 
+{
+  #arroja una excepción
+  Throw "The source directory $Path does not exist, please specify an existing directory"
+}
+
+#obtenemos la fecha
 $date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
-Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+
+#evaluamos una expresion dentro del string
+$DestinationFile = "$($DestinationPath + 'backup-' + $date + '.zip')"
+If (-Not (Test-Path $DestinationFile)) 
+{
+  #comprime el archivo
+  Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
+  
+  Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+} Else {
+  Write-Error "Today's backup already exists"
+}
